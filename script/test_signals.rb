044a8cb96a1ac70a7ebf18df03f93383def5fed4
A Faraday <andrewfaraday@hotmail.co.uk>
2013-11-27 22:33:23 +0000

    ropey first look drum sounds

--- script/test_signals.rb
+++ script/test_signals.rb
@@ -1,24 +1,24 @@
 require './lib/player.rb' 
 
 # straight forward note sequence
-#p = Player.new('sequences/test/notes.csv')
-#p.play
-#p.close
+p = Player.new('sequences/test/notes.csv')
+p.play
+p.close
 
 # volume per chanel
-#p = Player.new('sequences/test/volume.csv')
-#p.play
-#p.close
+p = Player.new('sequences/test/volume.csv')
+p.play
+p.close
 
 # master volume (across all channels) 
-#p = Player.new('sequences/test/master_volume.csv')
-#p.play
-#p.close
+p = Player.new('sequences/test/master_volume.csv')
+p.play
+p.close
 
 # volume envelopes (ADSR)
-#p = Player.new('sequences/test/envelopes.csv')
-#p.play
-#p.close
+p = Player.new('sequences/test/envelopes.csv')
+p.play
+p.close
 
 # drum line, testing event columns
 p = Player.new('sequences/test/drums.csv')
